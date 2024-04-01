f7e378a76cc22aed8fa1d6df5c18b8a732161762
Rodrigo Lucas <111752284+Rodrigolucsz@users.noreply.github.com>
2024-04-01 11:29:32 -0300

    q2.cpp

---
+++ q2.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/*
+**    Função : Encontrar Fatorial Duplo
+**    Autor : Rodrigo Lucas
+**    Data  : 01/04/2024
+**    Observações:  Tabela demonstrativa de fatorial duplo, fatorial normal e a diferença dos dois
+*/
+
+int fatorial_duplo(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    return n * fatorial_duplo(n - 2);
+}
+
+int fatorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    return n * fatorial(n - 1);
+}
+
+int main() {
+    printf(" n | Fatorial Duplo | Fatorial | Diferença\n");
+    printf("-------------------------------------------------\n");
+    for (int i = 2; i <= 20; i += 2) {
+        int fd = fatorial_duplo(i);
+        int f = fatorial(i);
+        int dif = f - fd;
+        printf(" %d | %10d | %10d | %10d\n", i, fd, f, dif);
+    }
+    return 665;
+}
